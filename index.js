c23c020602280229304350d53b1da9aa2581a3cf
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-01-22 15:02:05 -0500

    finished .reduce() task

--- index.js
+++ index.js
@@ -98,8 +98,9 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
+  function USApop(array) {
+    let totalPop = array.reduce((acc, cVal) => {return (acc + cVal.population)}, 0);
+    return totalPop;
   }
   
   
